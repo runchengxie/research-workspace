4cf30667c3761a1190711f8f1bf1797e2967f40e
Richard <709090316@qq.com>
2026-08-23 20:23:01 +0800

    feat(style-factors): export public fund loader

--- strategy-research/style_factors/loaders/__init__.py
+++ strategy-research/style_factors/loaders/__init__.py
@@ -7,6 +7,7 @@ are re-exported from this package.
 
 from __future__ import annotations
 
+from .fund_portfolio import load_fund_portfolio_features, materialize_fund_portfolio_state
 from .holder_structure import load_holder_structure
 from .limit_list import load_limit_list
 from .margin import load_margin
@@ -14,10 +15,12 @@ from .moneyflow_ths import load_moneyflow_ths
 from .sw_industry import load_sw_industry_membership, load_ths_member
 
 __all__ = [
+    "load_fund_portfolio_features",
     "load_holder_structure",
     "load_limit_list",
     "load_margin",
     "load_moneyflow_ths",
     "load_sw_industry_membership",
     "load_ths_member",
+    "materialize_fund_portfolio_state",
 ]
