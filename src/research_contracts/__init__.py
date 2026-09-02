df8c91d51cf21257a89f803ba7e36c2b22ce2204
Richard <709090316@qq.com>
2026-09-02 13:37:39 +0800

    feat: export platform publication contract

--- src/research_contracts/__init__.py
+++ src/research_contracts/__init__.py
@@ -28,6 +28,13 @@ from .file_receipts import (
     file_sha256,
     validate_file_receipts,
 )
+from .platform_publication import (
+    PLATFORM_PUBLICATION_SCHEMA_VERSION,
+    PUBLICATION_AUDIENCES,
+    PlatformPublicationArtifact,
+    PlatformPublicationManifest,
+    load_platform_publication_manifest,
+)
 
 __all__ = [
     "ARTIFACT_ENVELOPE_KEY",
@@ -35,12 +42,16 @@ __all__ = [
     "CORE_ARTIFACTS",
     "FILE_RECEIPT_SCHEMA_VERSION",
     "KNOWN_REPOS",
+    "PLATFORM_PUBLICATION_SCHEMA_VERSION",
+    "PUBLICATION_AUDIENCES",
     "ArtifactEnvelopeV2",
     "ArtifactContractManifest",
     "ContractValidationResult",
     "FileReceipt",
     "LegacyArtifactMetadata",
     "LineageInput",
+    "PlatformPublicationArtifact",
+    "PlatformPublicationManifest",
     "ProducerIdentity",
     "TargetHandoffContext",
     "attach_artifact_envelope_v2",
@@ -49,6 +60,7 @@ __all__ = [
     "file_receipt_payload",
     "file_sha256",
     "load_artifact_contract_manifest",
+    "load_platform_publication_manifest",
     "read_artifact_envelope",
     "validate_artifact_contract_manifest",
     "validate_file_receipts",
